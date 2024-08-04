aaa5d2c284897051d97152dd3ea49214d44d7889
Pedro Lopes <pedrolopescef@gmail.com>
2024-08-04 09:55:57 -0300

    NavBar Changes

--- conservidro/src/components/NavBar.js
+++ conservidro/src/components/NavBar.js
@@ -3,21 +3,56 @@ import {Link} from 'react-router-dom';
 import Logo from "../images/Logo.png";
 import styled from "styled-components";
 
-import {useState} from 'react';
+import { LuMenu } from "react-icons/lu";
+
+import React, {useState} from 'react';
 
 function NavBar(){
-    var initialPosition = window.scrollY;
+    const [navDisplay, setNavDisplay] = useState("hidden");
+    const [navHeight, setNavHeight] = useState("0");
+
+    function generateHr(){
+        if(window.innerWidth<728){
+            return(<hr className="my-4 w-24"/>);
+        }
+        else{
+            return (<br></br>)
+        }
+    }
+
+    function toggleNavBar(){
+        
+    }
+
 
 
     return(
-        <header className='w-screen h-24 fixed bg-black z-40'>
+        <header className={`w-screen md:h-24 fixed bg-black z-40`}>
             <nav className="flex h-full w-full justify-center items-center">
-                <ul className="flex h-full w-11/12">
-                    <div className='h-full w-1/3'>
-                        <Link to="/Conservidro/"><img className="flex-none h-full" src={Logo}></img></Link>
+                <ul className="flex h-full md:w-11/12 w-full md:flex-row flex-col items-center">
+                    <div className='md:h-full h-1/3 w-full md:w-1/3  m-auto flex justify-around items-center'>
+                        <Link to="/Conservidro/"><img className="flex-none size-24" src={Logo}></img></Link>
+                        
+                
+                        <LuMenu color="white" className='size-8 cursor-pointer md:hidden' onClick={()=>{
+                            let navBar = document.getElementById("navBar");
+                            if(navHeight == "0"){
+                                setNavDisplay("flex");
+                                navBar.classList.add("h-48");
+                                setNavHeight("48");
+                            }
+                            else{
+                                navBar.classList.remove("h-48");
+                                setNavHeight("0")
+                                setNavDisplay("hidden")
+                            }
+                        }}/>
                     </div>
-                    <div className="flex-grow flex justify-end items-center
-                    w-2/3 flex-row">
+                    
+                    <div id="navBar" className={`h-${navHeight} ${navDisplay} 
+                    md:flex flex-grow md:justify-end justify-around items-center
+                    md:w-2/3 w-full md:h-full flex-row md:flex-row flex-col 
+                    transition-all`}>
                         <li className="text-white mx-4 text-center">
                             <Link to="/Conservidro/sobre-nos" className="px-4 py-4 hover:text-black hover:bg-blue-400 rounded-lg transition-all">Saiba mais</Link>
                         </li>
@@ -29,6 +64,7 @@ function NavBar(){
                         </li>
                     </div>
                 </ul>
+
             </nav>
         </header>
     )
